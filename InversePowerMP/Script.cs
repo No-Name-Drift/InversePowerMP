57bd81a9b30e14d19348b122235ffa98f951194a
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-18 01:09:37 -0300

    Make the variables private

--- InversePowerMP/Script.cs
+++ InversePowerMP/Script.cs
@@ -7,14 +7,14 @@ namespace InversePowerMP
 {
     public class InversePower : BaseScript
     {
-        readonly float Power = API.GetConvarInt("ip_power", 100) / 100f;
-        readonly float Torque = API.GetConvarInt("ip_torque", 80) / 100f;
+        private readonly float Power = API.GetConvarInt("ip_power", 100) / 100f;
+        private readonly float Torque = API.GetConvarInt("ip_torque", 80) / 100f;
 
-        readonly float Angle = API.GetConvarInt("ip_angle", 350) / 100f;
-        readonly float Speed = API.GetConvarInt("ip_speed", 200) / 100f;
+        private readonly float Angle = API.GetConvarInt("ip_angle", 350) / 100f;
+        private readonly float Speed = API.GetConvarInt("ip_speed", 200) / 100f;
 
-        readonly float Base = API.GetConvarInt("ip_slope", 35) / 1f;
-        readonly float Deadzone = API.GetConvarInt("ip_deadzone", 0) / 1.0f;
+        private readonly float Base = API.GetConvarInt("ip_slope", 35) / 1f;
+        private readonly float Deadzone = API.GetConvarInt("ip_deadzone", 0) / 1.0f;
 
         public InversePower()
         {
