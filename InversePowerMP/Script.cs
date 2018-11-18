df9166a01078df6135c34bad72899c1b6728d2dd
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-18 00:00:12 -0300

    Added Configuration options

--- InversePowerMP/Script.cs
+++ InversePowerMP/Script.cs
@@ -6,6 +6,14 @@ namespace InversePowerMP
 {
     public class InversePower : BaseScript
     {
+        readonly float Power = API.GetConvarInt("ip_power", 100) / 100f;
+        readonly float Torque = API.GetConvarInt("ip_torque", 80) / 100f;
+
+        readonly float Angle = API.GetConvarInt("ip_angle", 350) / 100f;
+        readonly float Speed = API.GetConvarInt("ip_speed", 200) / 100f;
+
+        readonly float Slope = API.GetConvarInt("ip_slope", 35) / 1f;
+
         public InversePower()
         {
             Tick += OnTick;
