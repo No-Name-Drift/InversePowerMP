743fd1f1261de820bc206ab47ac8250ee301696b
Hannele Ruiz <justlemoncl@gmail.com>
2018-11-17 23:52:14 -0300

    Added basics

---
+++ InversePowerMP/Script.cs
@@ -0,0 +1,19 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System.Threading.Tasks;
+
+namespace InversePowerMP
+{
+    public class InversePower : BaseScript
+    {
+        public InversePower()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+
+        }
+    }
+}
